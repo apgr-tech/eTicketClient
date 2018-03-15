1ee6483b0964f9f573d6c2a543041181b5486716
ramiro <liko28s@gmail.com>
2018-03-15 16:10:06 -0500

    - Comit Inicial!

---
+++ app/server.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import polka from 'polka';
+import compression from 'compression';
+import sapper from 'sapper';
+import serve from 'serve-static';
+import fetch from 'node-fetch';
+import { routes } from './manifest/server.js';
+
+// this allows us to do e.g. `fetch('/api/blog-posts')` on the server
+global.fetch = (url, opts) => {
+	if (url[0] === '/') url = `http://localhost:${PORT}${url}`;
+	return fetch(url, opts);
+};
+
+// you can also use Express
+polka()
+	.use(compression({ threshold: 0 }))
+	.use(serve('assets'))
+	.use(sapper({ routes }))
+	.listen(process.env.PORT);
