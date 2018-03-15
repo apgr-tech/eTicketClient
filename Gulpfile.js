b1e7f49dd173e5df2261405df21933332a5830c5
ramiro <liko28s@gmail.com>
2018-03-15 17:25:31 -0500

    - global bulma styles - site menu styles

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var sourceMaps = require('gulp-sourcemaps');
 
 gulp.task('styles',function(){
-    gulp.src('sass/**/*.scss').
+    gulp.src('./sass/**/*.scss').
     pipe(sourceMaps.init()).
     pipe(sass().on('error', sass.logError)).
     pipe(sourceMaps.write()).
@@ -12,5 +12,5 @@ gulp.task('styles',function(){
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('./sass/**/*.scss',['styles']);
 });
