fe0b6687ba2dd8e35c6d936adf6c2c6153fe7041
ramiro <liko28s@gmail.com>
2018-03-22 19:09:42 -0500

    - global styles compilation task

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,6 +10,14 @@ gulp.task('styles',function(){
     pipe(gulp.dest('./assets/css/'))
 });
 
+gulp.task('global',function(){
+    gulp.src('./routes/_styles/global.scss').
+    pipe(sourceMaps.init()).
+    pipe(sass().on('error', sass.logError)).
+    pipe(sourceMaps.write()).
+    pipe(gulp.dest('./assets/'))
+});
+
 //Watch task
 gulp.task('default',function() {
     gulp.watch('./sass/**/*.scss',['styles']);
